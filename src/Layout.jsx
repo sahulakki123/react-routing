cadfeddbf318600d20e825eccfeb0c11d5567e7c
sahulakki123 <lakkisahus04@gmail.com>
2025-09-22 16:14:22 +0530

    Layout

---
+++ src/Layout.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import pic from "./assets/react.svg"
+
+
+const App=()=>{
+    return(
+
+        <>
+        <header>
+            <img src={pic} alt="" />
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">about</Link></li>
+                    <li><Link to="/serves">serves</Link></li>
+                    <li><Link to="/contact">contact</Link></li>
+                </ul>
+
+            </nav>
+        </header>
+        <Outlet/>
+
+        <h1>
+            Footer
+        </h1>
+
+        </>
+
+    )
+}
+
+export default App
--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import { Routes, Route, Link,  } from "react-router-dom";
-import pic from "./assets/react.svg"
+
 import Rhome from "./Rhome"
 import Rabout from "./Rabout"
 import Rserves from "./Rserves"
+import RContact from "./RContact"
+import Layout from "./Layout"
 
 
 const App=()=>{
@@ -12,19 +14,15 @@ const App=()=>{
     return(
         <>
         <h1>Routing</h1>
-        <header>
-            <img src={pic} alt="" />
-                            <ul>
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to="/about">about</Link></li>
-                                <li><Link to="/serves">serves</Link></li>
-                                <li><Link to="/dksnj">dksnj</Link></li>
-                            </ul>
-        </header>
             <Routes>
+                
+                <Route path="/" element={<Layout/>}>
+                <Route index element={<Rhome/>}/>
                 <Route path="/" element={<Rhome/>}/>
                 <Route path="/about" element={<Rabout/>}/>
                 <Route path="/serves" element={<Rserves/>}/>
+                <Route path="/contact" element={<RContact/>}/>
+            </Route>
             </Routes>
 
         </>
